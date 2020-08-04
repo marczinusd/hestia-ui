52cfa388efd3fac751ebbc101f21c86a24d231ca
David Marczinus <marczinus.david@gmail.com>
2020-08-04 12:28:08 +0200

    feat: wire up e2e test targets

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,5 +1,5 @@
-it("loads examples", () => {
-  const baseUrl: string = "http://localhost:4200";
+it('loads examples', () => {
+  const baseUrl: string = 'http://localhost:4200';
   cy.visit(baseUrl);
-  cy.contains("Replace me with something relevant");
+  cy.contains('hestia-ui app is running!');
 });
