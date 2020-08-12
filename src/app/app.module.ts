6c2ea9dd381e7b5b03394b07d5a862229598ffde
David Marczinus <marczinus.david@gmail.com>
2020-08-12 23:20:17 +0200

    feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { fakeBackendProvider } from './fakes/fake-backend-interceptor';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SnapshotDetailsComponent } from './components/snapshot-details/snapshot-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PageNotFoundComponent, DashboardComponent, SnapshotDetailsComponent],
-  imports: [BrowserModule, AppRoutingModule, MenubarModule],
+  imports: [BrowserModule, AppRoutingModule, MenubarModule, BrowserAnimationsModule],
   providers: [fakeBackendProvider],
   bootstrap: [AppComponent]
 })
