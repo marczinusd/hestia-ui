6e6e45babb91f21e34a53c1138b9574486e2340f
David Marczinus <marczinus.david@gmail.com>
2020-11-16 23:21:12 +0100

    test: add cypress fixtures and bump version

--- cypress/integration/dashboard.spec.ts
+++ cypress/integration/dashboard.spec.ts
@@ -1,6 +1,14 @@
+/// <reference types="cypress"/>
+
 describe('Dashboard', () => {
   beforeEach(() => {
     cy.visit('/dashboard');
+
+    cy.server();
+
+    cy.route('GET', '/snapshots', 'fixture:snapshots.json');
+    cy.route('GET', '/snapshots/*/files', 'fixture:files.json');
+    cy.route('GET', '/files/*', 'fixture:file-details.json');
   });
 
   it('shows snapshots header', () => {
