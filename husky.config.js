3c2be58a1908d64ddecde6e415521b2b8593667a
David Marczinus <marczinus.david@gmail.com>
2020-11-17 02:18:56 +0100

    build: add quiet e2e execution to pre-push hook

--- husky.config.js
+++ husky.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   hooks: {
     'pre-commit': 'lint-staged',
-    'pre-push': 'npm run ng lint && npm run test:coverage',
+    'pre-push': 'npm run ng lint && npm run test:coverage && npm run e2e:ci:quiet',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   }
 };
